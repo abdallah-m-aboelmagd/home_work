6537c748c9380f9fecfa21bf5d90d215ad878d0b
abdallah-m-aboelmagd <abdallah.m.aboelmagd@gmail.com>
2025-03-10 00:28:45 +0200

    first commit

---
+++ home_work/session1/Q12.dart
@@ -0,0 +1,6 @@
+/*Identify the data type of each of the following values in Dart: - 3.14 - "Hello" - true -
+42*/
+//3.14 : double
+//"Hello" : string
+//true : bool
+//42 : int
---
+++ home_work/session1/Q7.dart
@@ -0,0 +1,9 @@
+/*Given this code, identify any errors and explain why they occur: ```dart void main()
+{ String name = "Alice"; name = 123; print(name); } ```*/
+void main()
+{ 
+  String name = "Alice"; 
+//name = 123;
+// error because name type is defined as string and cant accept integer
+  print(name); 
+}
