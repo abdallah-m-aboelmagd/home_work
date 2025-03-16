91437385ff84a5463d7e55bc9ce4f62d70d88a33
AbdAllah AboElMagd <abdallah.m.aboelmagd@gmail.com>
2025-03-16 23:10:37 +0200

    Delete S3 directory

--- S3/q4.dart
+++
@@ -1,9 +0,0 @@
-// Objective: Understand the for-in loop for iterating through collections.
-// Task: Write a program that defines a list of integers and prints each number using a for-in loop.
-void main()
-{
-  List<int> ages = [3 , 4 , 5 , 6];
-  for(var age in ages){
-    print(age);
-  }
-}
